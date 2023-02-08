6aca9e460dc69464835b6e5bc57b143e7e187d47
Estefan Gonzales <estefan.dev@gmail.com>
2023-02-07 19:56:24 -0700

    Fixing chi-statistic

--- chi_square.py
+++ chi_square.py
@@ -4,16 +4,21 @@ import scipy
 
 def chi_square_attr_val(attribute, classification):
     attr_map = defaultdict(lambda: defaultdict(int))
+    class_map = defaultdict(int)
     for val, cls in zip(attribute, classification):
         attr_map[val][cls] += 1
+        class_map[cls] += 1
+    prob_e = class_map['e'] / len(classification)
+    prob_p = class_map['p'] / len(classification)
     chi_val = 0
     for v in attr_map:
         total = attr_map[v]['e'] + attr_map[v]['p']
-        expected = 0.5
-        var_e = ((attr_map[v]['e'] / total) - expected) ** 2
-        var_p = ((attr_map[v]['p'] / total) - expected) ** 2
-        chi_e = (var_e / expected)
-        chi_p = (var_p / expected)
+        expected_e = total * prob_e
+        expected_p = total * prob_p
+        var_e = (attr_map[v]['e'] - expected_e) ** 2
+        var_p = (attr_map[v]['p'] - expected_p) ** 2
+        chi_e = (var_e / expected_e)
+        chi_p = (var_p / expected_p)
         chi_val += (chi_p + chi_e)
 
     return chi_val
